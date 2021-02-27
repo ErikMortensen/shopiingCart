62d1be7e4ce0ce4a29432041021dadaf2704f397
Erik Mortensen <erikmortensen@Eriks-Air.fibertel.com.ar>
2021-02-27 13:01:37 -0300

    Iniciando carrito

---
+++ js/app.js
@@ -0,0 +1,92 @@
+// Variables
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+let articulosCarrito = [];
+
+cargarEventListeners();
+
+function cargarEventListeners(){
+    listaCursos.addEventListener('click', agregarCurso);
+    carrito.addEventListener('click', eliminarCurso);
+}
+
+
+function agregarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+function eliminarCurso(e){
+    console.log(e.target);
+}
+
+function leerDatosCurso(curso){
+    
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    if(articulosCarrito.some(curso => curso.id == infoCurso.id)){
+        const cursos = articulosCarrito.map( curso => {
+            if(curso.id == infoCurso.id){
+                curso.cantidad++;
+                return curso;
+            } else {
+                return curso;
+            }
+        });
+        articulosCarrito = [...cursos];
+    } else {
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+    
+    carritoHTML();
+}
+
+function carritoHTML(){
+
+    // Limpiar el HTML del body del carrito
+    limpiarHTML();
+
+    // Recorre y genera el HTML del body del carrito
+    articulosCarrito.forEach( curso => {
+        const { imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = ` 
+            <td>
+                <img src="${imagen}" width="100">
+            </td>
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
+            </td>
+        `;
+
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+function limpiarHTML(){
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
